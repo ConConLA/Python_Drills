045ef37a2be4ffd2ee65de5219056069589c2ff7
Lo <4864137-lotechsteamlabs@users.noreply.gitlab.com>
2025-06-03 10:07:38 -0700

    Update snippet

---
+++ 21_dictionaries.py
@@ -0,0 +1,99 @@
+# DT-21 DICTIONARIES
+# Points: 10
+# ----------------------------
+# DEFINITION: An key/value store of data 
+# 
+# 1. Key and value are required, together. 
+# 2. Begin and end with curly brackets {}
+# 3. Keys are unique.
+# 
+# Read: https://realpython.com/python-dicts/
+# =============================
+
+# A dictionary is a key/value store. 
+# Dictionary keys are unique -- they cannot be duplicated. 
+foods_list = []
+food_emoji = {
+    'orange': '🍊',
+    'apple': '🍎',
+    'lemon': '🍋',
+    'watermelon': '🍉',
+    'grapes': '🍇',
+    'avocado': '🥑',
+    'kiwi': '🥝',
+    'carrot': '🥕',
+    'corn': '🌽'
+}
+
+
+# TASK 1: INTRO
+# Create a new dictionary named contact. 
+
+
+# Add the following properties: first_name, last_name, address
+# Set their values to your name and address
+
+
+# Add a new property: age whose value is your age. 
+
+
+# Change the value of address to 2323 Broadway
+
+
+# Add a new property, city whose value is Oakland
+
+
+# Add anew property state whose value is California. 
+
+
+# Print the keys of the dictionary. 
+
+
+# Print the values of the dictionary. 
+
+
+# TASK 2: FOOD DICTIONARIES
+# Create a new dictionary for the foods below with the following properties:
+# name, is_sweet, emoji
+# Set their values accordingly. Use the food emoji dictionary.
+
+
+# Create a new dictionary named apple with the same properties. 
+# Add it to the food list.
+
+# Create a new dictionary named lemon with the same properties. 
+# Add it to the food list.
+
+
+# Create a new dictionary named watermelon with the same properties. 
+# Add it to the food list.
+
+
+# Create a new dictionary named grapes with the same properties.
+# Add it to the food list.
+
+
+# Create a new dictionary named corn with the same properties.
+# Add each fruit to the food list. 
+
+
+# Create a new dictionary named avocado with the same properties.
+# Add each fruit to the food list. 
+
+
+# Create a new dictionary named carrot with the same properties.
+# Add each fruit to the food list. 
+
+
+# Print the name and emoji for each food that is sweet.
+
+
+# Print the emoji for each food that is red. 
+
+
+# Print the emoji for each food that contains the letter 'o'
+
+
+# Print all the properties of each food in a nicely formatted row 
+# EX.: 🥬 Lettuce Green 
+
