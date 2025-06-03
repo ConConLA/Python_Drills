572064e31b45bad055e8769afe32355dc4979f4c
Lo <4864137-lotechsteamlabs@users.noreply.gitlab.com>
2025-06-03 10:05:48 -0700

    Initial commit

---
+++ 20_lists.py
@@ -0,0 +1,75 @@
+# DT-20 LISTS
+# Points: 10
+# ----------------------------
+# DEFINITION: An ordered group of objects. 
+# 
+# 1. Changeable. 
+# 2. Lists are begin and end with square brackets []
+# 
+# Read: https://realpython.com/python-lists/
+# =============================
+
+colors: list = ['red', 'green', 'blue', 'yellow', 'black', 'gray', 'white', 'orange']
+numbers: list[int] = [32, -7, 803, 2, 0, -107, 55, 231, 10042, 8, -42, 629, 20089]
+
+# MULTI-DIMENSIONAL LISTS
+# This is a 2-D list. It's a list with 2 lists inside.
+fish: list[str] = [
+    ['bass', 'catfish', 'trout', 'shad', 'sunfish', 'crappie', 'rainbow trout'],
+    ['blackbass', 'rainbow trout', 'mosquito fish', 'gudgeon', 'black bullhead', 'trout', 'carp']
+]
+
+# FLATTENING LISTS
+# To flatten a multi-dimensional list, take each item from all lists
+# within and put them into a single list. 
+
+# TASK 1
+# Sort the colors list in alphabetical order. Print it. 
+# Print each list backwards
+
+# Sort the numbers list from greatest to lowest. Print it. 
+
+
+# Print every 3rd item from each list
+
+
+# Print every 2nd item of each list, starting at the 2nd item. 
+
+
+# Print the last 2 items of each list. 
+
+
+# Using the numbers list, create a new list containing only negative numbers. 
+
+
+# Using the numbers list, create a new list containing only positive numbers. 
+
+
+# Using the colors list, create a new list containing colors that contain ... 
+# - the letter 'a' 
+# - the letter 'l' 
+# - 4 or less characters
+
+# Combine colors and numbers into 1 new list. Print the new list. 
+
+# TASK 2
+# Print the first item in fish in the fish list.
+
+
+# Print each fish capitalized.
+
+
+# Print the names of fish with names longer thant 5 characters.
+ 
+
+# Print the names of fish that start with the letter 'c' 
+
+
+# Create a new fish list that contains fish found in all the lists in the fish list. 
+
+
+# Using the colors list, create and print a new list for each item. 
+# The new list should contain each character of the item. 
+# EX. ['pink'] -> ['p', 'i', 'n', 'k']
+
+
